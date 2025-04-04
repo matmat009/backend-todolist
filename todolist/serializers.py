4300dc668dede625885af61515a800a865870e43
matmat009 <vashdestampede0009@gmail.com>
2025-04-04 22:43:17 +0800

    BACKEND

---
+++ todolist/serializers.py
@@ -0,0 +1,8 @@
+# todolist/serializers.py
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'title', 'completed', 'created_at']
